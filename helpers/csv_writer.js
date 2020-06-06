ea9eb0b11c4b21de469ae783affd00335a31b671
Jahmal Lewis <Jahmal23@gmail.com>
2020-06-06 11:17:18 -0400

    Added unit test

--- helpers/csv_writer.js
+++ helpers/csv_writer.js
@@ -1,14 +1,21 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-exports.writeCSV = async (header, path, records) => {
+exports.writeCSV = async (header, path, records, writer = null) => {
+    
+    if (records.length == 0) {
+        console.log('no records, skipping csv creation');
+        return false;
+    }
 
-    const csvWriter = createCsvWriter({
+    let csvWriter = writer ? writer : createCsvWriter({
         path: path,
         header: header
     });
-     
+
     await csvWriter.writeRecords(records)  
         .then(() => {
             console.log('...Done creating CSV');
         });
+
+    return true;
 }
